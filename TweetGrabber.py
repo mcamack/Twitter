b3b15a2087241064ef95020439c4ac3c25c231a5
Matt Camack <mcamack@gmail.com>
2017-02-26 17:22:53 -0800

    initial upload of files

---
+++ TweetGrabber.py
@@ -0,0 +1,48 @@
+import sys
+import tweepy
+from tweepy import OAuthHandler
+from tweepy import StreamListener
+from tweepy import Stream
+
+access_token = "show me YOUR access_token"
+access_token_secret = "show me YOUR access_token_secret"
+consumer_key = "show me YOUR consumer_key"
+consumer_secret = "show me YOUR consumer_secret"
+
+def process_or_store(tweet):
+	print(json.dumps(tweet))
+
+class MyListener(StreamListener):
+	def on_data(self, data):
+		try:
+			#with open('tweets.json', 'a') as f:
+			fname = sys.argv[1]
+			with open(fname, 'a') as f:
+				f.write(data)
+				return True
+		except BaseException as e:
+			print("Error on_data: %s" % str(e))
+		return True
+
+	def on_error(self, status):
+		print(status)
+		return True
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+twitter_stream = Stream(auth, MyListener())
+search_term = sys.argv[2]
+twitter_stream.filter(track=['Trump'])
+
+#search_results = tweepy.Cursor(api.search, q='wwe').items()
+#for tag in search_results:
+#	print(tag.text)
+
+#for status in tweepy.Cursor(api.home_timeline).items(1):
+#	print(status.text)
+
+
+#for friend in tweepy.Cursor(api.friends).items():
+	#process_or_store(friend._json)
+	#print(friend.name)
---
+++ jsonParser.py
@@ -0,0 +1,25 @@
+import sys
+import operator
+import json
+from collections import Counter
+import nltk
+
+#fname = 'tweets.json'
+fname = sys.argv[1]
+
+with open(fname, 'r') as f:
+	count_all = Counter()
+	for line in f:
+		tweet = json.loads(line)
+
+		tweet = tweet.get('text')
+		if(tweet is not None):
+			#print(tweet)
+			words = tweet.split()
+			#print(words)
+
+			terms_all = [term for term in words if len(term)>5]
+			count_all.update(terms_all)
+			#print(count_all.most_common(10))
+
+print(nltk.FreqDist(terms_all).keys())
